c8da9d3a95baba5c41240ddca7aad1e613f8cdba
AlexBMA <burghelea.alex@gmail.com>
2017-09-12 23:23:58 +0300

    recursive version of the fibonacci function added

--- src/problem/pack/one/Problem3.kt
+++ src/problem/pack/one/Problem3.kt
@@ -9,7 +9,9 @@ package problem.pack.one
 
 fun main(args: Array<String>) {
 
-    print(calculateFibonacci(10))
+    println(calculateFibonacci(10))
+
+    println(calculateFibonacciRecursive(10, 0, 1))
 }
 
 fun calculateFibonacci(n: Int): Int {
@@ -24,4 +26,10 @@ fun calculateFibonacci(n: Int): Int {
         b = c
     }
     return c
+}
+
+fun calculateFibonacciRecursive(n: Int, a: Int = 0, b: Int = 1): Int {
+    if (n == 0) return a
+    if (n == 1) return b
+    return calculateFibonacciRecursive(n - 1, b, a + b)
 }
