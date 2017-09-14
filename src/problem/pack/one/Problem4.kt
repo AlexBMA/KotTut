1d2565f7f3555cfee0756ce311e2b79d4c34270b
AlexBMA <burghelea.alex@gmail.com>
2017-09-14 23:42:41 +0300

    GCD problem using euclid alg

---
+++ src/problem/pack/one/Problem4.kt
@@ -0,0 +1,26 @@
+package problem.pack.one
+
+/*
+    Greatest common divisor (GCD) of two numbers
+    using euclid algorithm
+ */
+fun main(args: Array<String>) {
+
+    val a = 21
+    var b = 30
+
+    print(gcdEuclid(a, b))
+}
+
+fun gcdEuclid(a: Int, b: Int): Int {
+    if (a == 0) return b
+    if (b == 0) return a
+    
+    if (a > b) {
+        val r = a % b
+        return gcdEuclid(b, r)
+    } else {
+        var r = b % a
+        return gcdEuclid(a, r)
+    }
+}
